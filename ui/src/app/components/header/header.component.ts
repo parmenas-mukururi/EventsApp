34f43d4fc4e35d3cec251b663a00d8d829790c8e
parmenas-mukururi <machariaparmenas22@gmail.com>
2024-09-27 13:09:42 +0300

    Add token decoding Modify auth service Remove unused packages

--- ui/src/app/components/header/header.component.ts
+++ ui/src/app/components/header/header.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  user: any = null;
 
+  constructor(private router: Router, private authService: AuthService) {}
+  navigateToLoginPage() {
+    this.router.navigateByUrl('login');
+  }
+
+  logout() {
+    this.authService.logout();
+    this.user = null;
+    this.navigateToLoginPage();
+  }
+
+  ngOnInit(): void {
+    this.user = this.authService.getUserInfo();
+  }
 }
--- ui/src/app/services/auth/auth.service.ts
+++ ui/src/app/services/auth/auth.service.ts
@@ -1,19 +1,45 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import  {jwtDecode}  from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private userSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+  public user$: Observable<any> = this.userSubject.asObservable();
+
   loginService(data: object): Observable<any> {
     return this.http.post('https://localhost:7225/api/Auth/login', data);
   }
 
+  logout() {
+    localStorage.removeItem('access_token');
+  }
   getUserToken() {
     const token = localStorage.getItem('access_token');
     return token;
   }
 
+  getDecodedToken(token: string | null) {
+    if (token) {
+      const decoded = jwtDecode(token);
+      console.log(decoded);
+      return decoded;
+    }
+    return null;
+  }
+
+  getUserInfo() {
+    const token = this.getUserToken()
+    const decoded = this.getDecodedToken(token);
+    return decoded;
+  }
+
+
+  isLoggedIn() {
+    return localStorage.getItem('access_token') !== null;
+  }
   constructor(private http: HttpClient) {}
 }
